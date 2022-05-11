8af9cee63dbf6aaa298ff8675d4d6520e0a81f1a
Tzvi-H <16guitar@gmail.com>
2022-05-11 13:05:43 -0600

    completed 9.b

--- multiplier.ts
+++ multiplier.ts
@@ -1,29 +1,37 @@
-// type Operation = "multiply" | "add" | "divide";
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
 
-// type Result = number;
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
 
-// const calculator = (a: number, b: number, op: Operation): Result => {
-//   switch (op) {
-//     case "multiply":
-//       return a * b;
-//     case "divide":
-//       if (b === 0) throw new Error("Can't divide by 0!");
-//       return a / b;
-//     case "add":
-//       return a + b;
-//     default:
-//       throw new Error("Operation is not multiply, add or divide!");
-//   }
-// };
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
 
-// try {
-//   console.log(calculator(1, 5, "divide"));
-// } catch (error: unknown) {
-//   let errorMessage = "Something went wrong.";
-//   if (error instanceof Error) {
-//     errorMessage += " Error: " + error.message;
-//   }
-//   console.log(errorMessage);
-// }
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+};
 
-console.log(process.argv);
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(
+    value1,
+    value2,
+    `Multiplied ${value1} and ${value2}, the result is:`
+  );
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
